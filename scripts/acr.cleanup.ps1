57e1ab92c972b028a5251bdef44f58164365610e
Razmik Minasyan <razmik.minasyann@gmail.com>
2022-08-16 09:44:32 +0400

    add comment

--- scripts/acr.cleanup.ps1
+++ scripts/acr.cleanup.ps1
@@ -1,1 +1,2 @@
 # This script check and removes the old docker images from the azure container registry
+# WARNING: this script will delete all image tags within a repository that share the same manifest
