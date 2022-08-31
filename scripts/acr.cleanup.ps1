73e8bb9e324e6a1c0519114d5dbc60a3196f1162
Razmik <razmik.minasyann@gmail.com>
2022-08-31 11:51:48 +0400

    add script

--- scripts/acr.cleanup.ps1
+++ scripts/acr.cleanup.ps1
@@ -41,3 +41,5 @@ if ($Repository) {
 }
 else {
     Write-Host "Getting list of all repositories in container registry: $AzureRegistryName"
+    $RepoList = az acr repository list --name $AzureRegistryName --output table
+}
